39a30f6d134a154aec311ce2c9fe03891f3e101a
sameer Uranw <110217057+Sameeruranw@users.noreply.github.com>
2024-02-26 22:37:47 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,60 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv")
-const authRoute = require("./routes/auth")
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+const cors=require('cors')
+const multer=require('multer')
+const path=require("path")
+const cookieParser=require('cookie-parser')
+const authRoute=require('./routes/auth')
+const userRoute=require('./routes/users')
+const postRoute=require('./routes/posts')
+const commentRoute=require('./routes/comments')
 
-const UserRoute = require("./routes/users")
-const cors = require("cors")
-const app = express();
-const postRoute = require("./routes/posts")
-const CommentRoute = require("./routes/comments");
-const cookieParser = require("cookie-parser");
-const multer = require("multer");
-const path = require("path");
-//middleware
+//database
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully!")
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+
+
+//middlewares
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
 app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
-app.use("/api/users",UserRoute)
+app.use("/api/users",userRoute)
 app.use("/api/posts",postRoute)
-app.use("/api/comments",CommentRoute)
+app.use("/api/comments",commentRoute)
 
 //image upload
-const storage = multer.diskStorage({
+const storage=multer.diskStorage({
     destination:(req,file,fn)=>{
         fn(null,"images")
     },
-    filename:(req,file,cb)=>{
-        cb(null,req.body.img)
-        //fn(null,"brother-logo.png")
+    filename:(req,file,fn)=>{
+        fn(null,req.body.img)
+        // fn(null,"image1.jpg")
     }
-}) 
-
-const upload = multer({storage:storage})
-app.post("/api/upload", upload.single("file"), (req, res) => {
-    res.status(200).json("Image has been successfully uploaded");
-});
-
-
-
-mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-    console.log("connected to database Successfully")
-}).then(()=>{
-    app.listen(process.env.port)
-}).catch((err)=>{
-    console.log(err)
 })
-//MIZ7Wu1AN6vBrTxk
+
+const upload=multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    // console.log(req.body)
+    res.status(200).json("Image has been uploaded successfully!")
+})
+
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    console.log("app is running on port "+process.env.PORT)
+})
